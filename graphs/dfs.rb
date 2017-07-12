6c9515548d17ad3f88de0fb90d3736b8005012b9
Matt Carlson <matthewthomascarlson@gmail.com>
2017-07-11 22:37:35 -0700

    add ruby depth first search

---
+++ graphs/dfs.rb
@@ -0,0 +1,25 @@
+require 'set'
+require 'pry'
+
+def dfs(graph, start, visited = nil)
+
+  unless visited
+    visited = Set.new
+  end
+  visited.add(start)
+  (graph[start.to_sym]-visited).each do |nxt|
+    dfs(graph, nxt, visited)
+  end
+  visited
+end
+
+# testing the method
+
+graph = {'A': Set.new(['B', 'C']),
+         'B': Set.new(['A', 'D', 'E']),
+         'C': Set.new(['A', 'F']),
+         'D': Set.new(['B']),
+         'E': Set.new(['B', 'F']),
+         'F': Set.new(['C', 'E'])
+}
+dfs(graph, 'A')
